aad252ee66c399ac6327c30b7389f1f0123e6912
Alex Asenov <alex.ace7ov@gmail.com>
2024-01-08 23:01:34 +0200

    Enter github endpoint address

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/alexace21/Smart-Reader", "_blank")
           }
           className='black_btn'
         >
